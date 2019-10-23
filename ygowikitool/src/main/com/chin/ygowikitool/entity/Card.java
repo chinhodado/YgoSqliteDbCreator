29416059aaffc8b2fc03b5b7979a6d8cb1d1bac8
Trung Do <chin.bimbo@gmail.com>
2019-10-22 20:37:38 -0400

    Merge with ygodb's Card class

--- ygowikitool/src/main/com/chin/ygowikitool/entity/Card.java
+++ ygowikitool/src/main/com/chin/ygowikitool/entity/Card.java
@@ -7,13 +7,53 @@ import java.util.List;
  * Created by Chin on 13-May-17.
  */
 public class Card {
-    private String realName = "", attribute = "", cardType = "", types = "", level = "", atk = "", def = "", passcode = "",
-            effectTypes = "", materials = "", fusionMaterials = "", rank = "", ritualSpell = "",
-            pendulumScale = "", linkMarkers = "", link = "", property = "", summonedBy = "", limitText = "", synchroMaterial = "", ritualMonster = "",
-            lore = "", ocgStatus = "", tcgAdvStatus = "", tcgTrnStatus = "",
-            img = "";
+    // name is the title of the article, which is guaranteed to be unique, but may not be the card's true name
+    // e.g. it may be missing #, have "(card)" at the end, etc.
+    private String name = "";
+
+    // realName is the real name of the card, but may not be unique (e.g. tokens, egyptian gods, etc.)
+    private String realName = "";
+    private String attribute = "";
+    private String cardType = "";
+    private String types = "";
+    private String level = "";
+    private String atk = "";
+    private String def = "";
+    private String passcode = "";
+    private String effectTypes = "";
+    private String materials = "";
+    private String fusionMaterials = "";
+    private String rank = "";
+    private String ritualSpell = "";
+    private String pendulumScale = "";
+    private String linkMarkers = "";
+    private String link = "";
+    private String property = "";
+    private String summonedBy = "";
+    private String limitText = "";
+    private String synchroMaterial = "";
+    private String ritualMonster = "";
+    private String lore = "";
+    private String ocgStatus = "";
+    private String tcgAdvStatus = "";
+    private String tcgTrnStatus = "";
+    private String img = "";
+    private String fullImgLink = "";
 
     private List<String> archetypes = new ArrayList<>();
+
+    // these comes from the booster card table
+    private String setNumber = "";
+    private String rarity = "";
+    private String category = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getRealName() {
         return realName;
@@ -238,5 +278,103 @@ public class Card {
 
     public void setImg(String img) {
         this.img = img;
+    }
+
+    public String getFullImgLink() {
+        return fullImgLink;
+    }
+
+    public void setFullImgLink(String fullImgLink) {
+        this.fullImgLink = fullImgLink;
+    }
+
+    public String getSetNumber() {
+        return setNumber;
+    }
+
+    public void setSetNumber(String setNumber) {
+        this.setNumber = setNumber;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "<b>" + getDisplayName() + "</b><br>";
+        if (!category.equals("")) {
+            tmp += small(category) + "<br>";
+        }
+        if (cardType.equals("Spell") || cardType.equals("Trap")) {
+            tmp += small(property + " " + cardType);
+        }
+        else {
+            tmp += "<small>";
+            if (!level.equals("")) {
+                tmp += "Level " + level;
+                if (!pendulumScale.equals("")) {
+                    tmp += ", Pendulum Scale " + pendulumScale;
+                }
+                tmp += "<br>";
+            }
+            else if (!rank.equals("")) {
+                tmp += "Rank " + rank + "<br>";
+            }
+            tmp += (attribute + " " + types + "<br>" + atk + "/" + (!link.equals("")? "LINK " + link : def) + "</small>");
+        }
+
+        if (!setNumber.equals("")) {
+            tmp += "<br>" + small(setNumber);
+            if (!rarity.equals("")) {
+                tmp += small(" (" + rarity + ")");
+            }
+        }
+
+        return small(tmp);
+    }
+
+    private String small(String text) {
+        return "<small>" + text + "</small>";
+    }
+
+    private String smaller(String text) {
+        return "<small>" + small(text) + "</small>";
+    }
+
+    /**
+     * Get the display name for the card. Most of the time this is the card's "name" property,
+     * which is its article name on wikia. However when this name is different from the card's
+     * real name, we may want to use the real name instead. Note that we don't always want to
+     * display the real name because the article name can be useful (e.g. all tokens's real name
+     * is the same).
+     * @return a name suitable to be displayed
+     */
+    public String getDisplayName() {
+        String displayName = name;
+
+        // for cards like Jinzo #7 where the # is always missing from the article name
+        if (realName != null && realName.contains("#")) {
+            displayName = realName;
+        }
+
+        // for cards with article name like "Pharaoh's Servant (card)"
+        if (realName != null && !realName.equals("") && name.endsWith("(card)")) {
+            displayName = realName;
+        }
+
+        return displayName;
     }
 }
